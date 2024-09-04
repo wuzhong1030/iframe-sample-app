5e4097acae4f4ade3515794437ada21c6f687a80
stone.wu <stone.wu@webeye.com>
2024-09-04 10:11:59 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+
+import { useState } from "react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@nextui-org/modal";
 import { Button } from "@nextui-org/button";
 
 export default function Home() {
+  const [isOpen, setisOpen] = useState(false);
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <Button>click me</Button>
+      <Button onClick={() => setisOpen(true)}>click me</Button>
+      <Modal size="full" isOpen={isOpen} onClose={() => setisOpen(false)} hideCloseButton disableAnimation>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <iframe
+                referrerPolicy="no-referrer"
+                style={{ width: "100vw", height: "100vh" }}
+                src="https://test.todaydealspro.com/v2/landing/index?userid=stonetest6_a_f&language=en&timezone=America/Chicago&appid=0fd4b145c5e34a17a6f1204be9b0cb21"
+              />
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </section>
   );
 }
