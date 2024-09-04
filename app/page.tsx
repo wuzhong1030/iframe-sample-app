a124579e5151c18c0e4684dde6897d3946af9a64
stone.wu <stone.wu@webeye.com>
2024-09-04 09:55:50 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@nextui-org/button";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <Button>click me</Button>
+    </section>
+  );
+}
